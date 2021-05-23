478fb65f447510b436ba7140ef1a488ea48c70ae
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-23 12:12:25 +0530

    Add files via upload

---
+++ Multiple Interfaces 3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Multiple_interface3
+{
+    interface ICustomer
+    {
+        void print();
+
+    }
+    interface customer : ICustomer              //interface can inherit from another interface
+    {
+        void Display();
+    }
+    class ClassA : customer
+    {
+        public void Display()
+        {
+            Console.WriteLine("print method from ClassA implimenting interface customer");
+        }
+        public void print()
+        {
+            Console.WriteLine("print method from ClassA implimenting interface Icustomer");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICustomer a = new ClassA();                     // Icustomer is parent to ClassA so reference variable of it can point to ClassA object
+            a.print();
+        }
+    }
+}
