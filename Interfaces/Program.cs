43ff2cd36213f10d5259ad22aeb79d20cc7743eb
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-23 11:41:17 +0530

    Add files via upload

---
+++ Interfaces/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Interfaces
+{
+    interface ICustomer
+    {
+        //int CID;                            //Error Interfaces cannot contain instance fields
+        void print();
+        //void display()                                //interface cannot contain implementation
+        //{
+        //    Console.WriteLine("Hello World!");
+        //}
+    }
+    //class ClassA : ICustomer                       //Error 'ClassA' does not implement interface member 'ICustomer.print()'
+    //{
+
+    //}
+    
+    class ClassB : ICustomer
+    {
+        //public void print(int num1, int num2)                     //Error 'ClassB' does not implement interface member 'ICustomer.print()'
+        //{ 
+            
+        //}
+        public void print()
+        {
+            Console.WriteLine("print method from ClassB implimenting interface Icustomer");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ICustomer c = new ICustomer();                  //Error Cannot create an instance of the abstract type or interface 'ICustomer'	
+            ClassB b = new ClassB();
+            b.print();
+        }
+    }
+}
