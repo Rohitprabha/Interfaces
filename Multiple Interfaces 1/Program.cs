1fac3ef5847ca200192926a3c635973225d25690
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-23 11:44:05 +0530

    Add files via upload

---
+++ Multiple Interfaces 1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Multiple_Interfaces
+{
+    interface ICustomer
+    {
+        void print();
+
+    }
+    interface customer
+    {
+        void Display();
+    }
+    class ClassA : ICustomer, customer                          //class can inherit from more than one interface at the same time
+    {
+        public void print()
+        {
+            Console.WriteLine("print method from ClassA implimenting interface Icustomer");
+        }
+        public void Display()
+        {
+            Console.WriteLine("print method from ClassA implimenting interface customer");
+        }
+    }
+    class ClassB
+    {
+
+    }
+    class Program : ClassA
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Display();
+            p.print();
+        }
+    }
+    //class Program : ClassA, ClassB                               //Error Class 'Program' cannot have multiple base classes: 'ClassA' and 'ClassB'
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        ClassA a = new ClassA();
+    //        a.print();
+    //        a.Display();
+    //    }
+    //}
+}
