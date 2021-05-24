14ab061507dffab24167d9583a29e4665a1b31e4
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-24 11:24:04 +0530

    Add files via upload

---
+++ Types/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Interface_Types
+{
+    interface ICustomer
+    {
+        void print();
+
+    }
+    interface customer
+    {
+        void print();
+    }
+    class ClassA : ICustomer, customer
+    {
+        //Implicit interface Implementation
+        public void print()
+        {
+            Console.WriteLine("print method from ClassA");
+        }
+
+        //Explicit interface Implementation
+        void ICustomer.print()
+        {
+            Console.WriteLine("print method from ClassA implimenting interface Icustomer");
+        }
+        void customer.print()
+        {
+            Console.WriteLine("print method from ClassA implimenting interface customer");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassA a = new ClassA();
+            a.print();
+
+            ICustomer b = new ClassA();
+            b.print();
+
+            customer c = new ClassA();
+            c.print();
+        }
+    }
+}
